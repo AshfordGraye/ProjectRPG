19505972b4be6baa9300b1d54f908bfb88718870
Ashford <barker.tokamak_0g@icloud.com>
2023-01-25 12:20:03 +0000

    Create basic world menu
    This menu will be inherited by each location. This allows the menu to be altered without writing the full menu for each area.

---
+++ MenuSystem.py
@@ -0,0 +1,27 @@
+from Characters import *
+
+# Location Menus. The menu items will be adjusted dynamically based on location
+class Location:
+
+    place = ("")
+    option1 = ("")
+    option2 = ("")
+    option3 = ("")
+    travel1 = ("")
+    travel2 = ("")
+    travel3 = ("")    
+    
+    def Menu():
+        MenuTitle.write (f"{Location.place}\n")
+        MenuTitle.write ("Action Menu")
+        PlayerInput.write (f"1: {Location.option1}")
+        PlayerInput.write (f"2: {Location.option2}")
+        PlayerInput.write (f"3: {Location.option3}")
+        MenuTitle.write ("Travel Menu")
+        PlayerInput.write (f"4: {Location.option1}")
+        PlayerInput.write (f"5: {Location.option1}")
+        PlayerInput.write (f"6: {Location.option1}")
+        MenuTitle.write ("Player Menu")
+        PlayerInput.write (f"7: Check Items")
+        PlayerInput.write (f"8: Check Weapons")
+        PlayerInput.write (f"9: Check Stats")
